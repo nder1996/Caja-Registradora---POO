9cf67305dc8d588e6e68283b3d551a0bbb5ce975
nder1996 <ander_2030@hotmail.com>
2020-12-06 22:55:06 -0500

    V19.0

--- PruebaUnitaria(Productos).py
+++ PruebaUnitaria(Productos).py
@@ -6,59 +6,47 @@ class Test_CajaRegistradora_Producto(unittest.TestCase):
 
     def test_Validar_Cantidad_True(self):
         Producto_Cliente = Producto(0,"500",[],[],[])
-        Validar  = Producto_Cliente.Validar_P()
-        self.assertTrue(True,bool(Validar) is True) 
+        self.assertTrue(True,bool(Producto_Cliente.Validar_P()) is True) 
     
     def test_Validar_Cantidad_False(self):
         Producto_Cliente = Producto(0,"500.7",[],[],[])
-        Validar  = Producto_Cliente.Validar_P()
-        self.assertFalse(False,bool(Validar) is False) 
+        self.assertFalse(False,bool(Producto_Cliente.Validar_P()) is False) 
 
     def test_Validar_Valor_True(self):
         Producto_Cliente = Producto(1,"500.35",[],[],[])
-        Validar  = Producto_Cliente.Validar_P()
-        self.assertTrue(True,bool(Validar) is True) 
+        self.assertTrue(True,bool(Producto_Cliente.Validar_P()) is True) 
 
     def test_Validar_Valor_False(self):
         Producto_Cliente = Producto(1,"500.7AS",[],[],[])
-        Validar  = Producto_Cliente.Validar_P()
-        self.assertFalse(False,bool(Validar) is False) 
+        self.assertFalse(False,bool(Producto_Cliente.Validar_P()) is False) 
 
     def test_Validar_Descuento_True(self):
         Producto_Cliente = Producto(2,"50.35",[],[],[])
-        Validar  = Producto_Cliente.Validar_P()
-        self.assertTrue(True,bool(Validar) is True) 
+        self.assertTrue(True,bool(Producto_Cliente.Validar_P()) is True) 
 
     def test_Validar_Descuento_False(self):
         Producto_Cliente = Producto(2,"50.7ASp",[],[],[])
-        Validar  = Producto_Cliente.Validar_P()
-        self.assertFalse(False,bool(Validar) is False) 
+        self.assertFalse(False,bool(Producto_Cliente.Validar_P()) is False) 
     
     def test_CrearProducto_ARROZ_None(self):
-        Articulos = ["A65SD6SD","1","ARROZ","100","10"] ; Sub_Total = [] ; Compra_Total = []
-        Sub_Total.append("500")
+        Articulos = ["A65SD6SD","1","ARROZ","100","10"] ; Sub_Total = [] ; Compra_Total = [] ; Sub_Total.append("500")
         Producto_Cliente = Producto(-1,"100",Sub_Total,Articulos,Compra_Total)
-        Validar = Producto_Cliente.Crear_Producto()
-        self.assertIsNone(None,Validar is  None) 
+        self.assertIsNone(None,Producto_Cliente.Validar_P() is  None) 
     
     def test_CrearProducto_PERA_None(self):
-        Articulos = ["123PERASDFDF","1","PERA","20","45"] ; Sub_Total = [] ; Compra_Total = []
-        Sub_Total.append("700")
+        Articulos = ["123PERASDFDF","1","PERA","20","45"] ; Sub_Total = [] ; Compra_Total = [] ; Sub_Total.append("700")
         Producto_Cliente = Producto(-1,"110",Sub_Total,Articulos,Compra_Total)
-        Validar = Producto_Cliente.Crear_Producto()
-        self.assertIsNone(None,Validar is  None)     
+        self.assertIsNone(None,Producto_Cliente.Validar_P() is  None)     
     
     def test_CrearProducto_ARROZ_NotNone(self):
         Articulos = ["A65SD6SD","1","ARROZ","100","10"] ; Sub_Total = [] ; Compra_Total = []
         Producto_Cliente = Producto(-1,"90",Sub_Total,Articulos,Compra_Total)
-        Validar = Producto_Cliente.Crear_Producto()
-        self.assertIsNotNone(Validar is not None) 
+        self.assertIsNotNone(Producto_Cliente.Validar_P() is not None) 
     
     def test_CrearProducto_PERA_NotNone(self):
         Articulos = ["A65SD6SD","1","PERA","200","45"] ; Sub_Total = [] ; Compra_Total = []
         Producto_Cliente = Producto(-1,"110",Sub_Total,Articulos,Compra_Total)
-        Validar = Producto_Cliente.Crear_Producto()
-        self.assertIsNotNone(Validar is not None)    
+        self.assertIsNotNone(Producto_Cliente.Validar_P() is not None)    
         
 
 if __name__ == "__main__":
--- PruebasUnitarias(Escaner).py
+++ PruebasUnitarias(Escaner).py
@@ -1,13 +1,24 @@
 from CajaRegistradora import Escaner
 import string , unittest
 
+class Test_CajaRegistradora_Escaner(unittest.TestCase):
+    
+    def setUp(self):
+        Compra_Total = ["|| {} UNIDADES DE {} TIENE UN DESCUENTO DE {} % \n\n|| SUB-TOTAL A PAGAR ES $ {:.3f} USD".format("1","ARROZ","10",90.00),
+                        "|| {} UNIDADES DE {} TIENE UN DESCUENTO DE {} % \n\n|| SUB-TOTAL A PAGAR ES $ {:.3f} USD".format("1","PERA","45",110.0)]
+        Sub_Total = ["90.0","100.0"]    
+        self.Escaner_P =Escaner(Sub_Total,Compra_Total) 
+    
+    def test_MostrarPedido_IsNotNone(self):
+        self.assertIsNotNone(self.Escaner_P.Mostrar_Pedido() is not None) 
 
+    def test_TotalPedido_IsNotNone(self):
+        Total = 0.0
+        self.assertIsNotNone(self.Escaner_P.Total_Pedido(Total) is not None) 
 
-
-
-
-
-
+    def test_EliminarArticulo_IsNotNone(self):
+        Total = 0.0
+        self.assertIsNotNone(self.Escaner_P.Total_Pedido(Total) is not None) 
 
 
 
